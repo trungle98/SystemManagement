bd23ce9df33c465694e19410713c4d00d8de23ea
trungle98 <37513675+trungle98@users.noreply.github.com>
2023-02-14 09:19:55 -0500

    first commit

---
+++ src/main/java/com/edu/greenwich/managementsystem/model/ERole.java
@@ -0,0 +1,7 @@
+package com.edu.greenwich.managementsystem.model;
+
+public enum ERole {
+    ROLE_USER,
+    ROLE_MODERATOR,
+    ROLE_ADMIN
+}
---
+++ src/test/java/com/edu/greenwich/managementsystem/ManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu.greenwich.managementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
