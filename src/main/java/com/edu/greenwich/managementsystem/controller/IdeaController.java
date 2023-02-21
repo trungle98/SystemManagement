e356d076cb44f3993885341bf43a61bc5053ccde
trungle98hn <trungle98hn@gmail.com>
2023-02-22 02:44:51 +0700

    add get file by name

--- src/main/java/com/edu/greenwich/managementsystem/controller/IdeaController.java
+++ src/main/java/com/edu/greenwich/managementsystem/controller/IdeaController.java
@@ -7,12 +7,19 @@ import com.edu.greenwich.managementsystem.dto.response.IdeaResponse;
 import com.edu.greenwich.managementsystem.dto.response.ResponseMessage;
 import com.edu.greenwich.managementsystem.model.Comment;
 import com.edu.greenwich.managementsystem.model.Idea;
+import com.edu.greenwich.managementsystem.service.StorageServceImpl;
 import com.edu.greenwich.managementsystem.service.StorageService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
@@ -22,6 +29,7 @@ import java.util.Optional;
 @RestController
 @RequestMapping("/api/idea")
 public class IdeaController {
+    private static Logger logger = LoggerFactory.getLogger(IdeaController.class);
     @Autowired
     IdeaRepository ideaRepository;
     @Autowired
@@ -42,6 +50,8 @@ public class IdeaController {
 
     @PostMapping("/save")
     public ResponseEntity<ResponseMessage> save(@ModelAttribute IdeaRequest ideaRequest) {
+        ServletRequestAttributes attr = (ServletRequestAttributes) RequestContextHolder.currentRequestAttributes();
+        logger.info("receive save request");
         String fileName ="";
         Idea savedIdea;
         try {
@@ -52,8 +62,10 @@ public class IdeaController {
             idea.setFileLocation(fileName);
             savedIdea = ideaRepository.save(idea);
         }catch (Exception e){
+            logger.error("save error caused ={}",e);
             return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED).contentType(MediaType.APPLICATION_JSON).body(new ResponseMessage("Error when upload file, please try again!"));
         }
+        logger.info("end request");
         return ResponseEntity.status(HttpStatus.ACCEPTED).contentType(MediaType.APPLICATION_JSON).body(new ResponseMessage(savedIdea));
     }
 
@@ -67,4 +79,11 @@ public class IdeaController {
         return commentRepository.findAllByIdeaId(ideaId);
     }
 
+    @GetMapping("/files/{filename:.+}")
+    @ResponseBody
+    public ResponseEntity<Resource> getFile(@PathVariable String filename) {
+        Resource file = storageService.load(filename);
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + file.getFilename() + "\"").body(file);
+    }
 }
